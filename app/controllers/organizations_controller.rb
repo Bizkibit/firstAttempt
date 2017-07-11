4e76575c7583263ccb5f192aafcd3c55c3ddb845
Farshid HM <fhmanesh@gmail.com>
2017-07-10 17:13:54 -0700

    added map to the index page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -34,6 +34,12 @@ class OrganizationsController < ApplicationController
 
   def index
     @organizations = Organization.all
+    @organization =  Organization.near([49.2819605,-123.1086604], 1000)
+    @hash = Gmaps4rails.build_markers(@organization) do |organization, marker|
+      marker.lat organization.latitude
+      marker.lng organization.longitude
+      marker.infowindow organization.name
+     end
   end
 
   def edit
